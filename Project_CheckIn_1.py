1776e636bac51b07ff55ffbc008c2fa62ac062db
Chloe Root <croot1@umd.edu>
2021-11-15 16:03:51 -0500

    created player class

---
+++ Project_CheckIn_1.py
@@ -0,0 +1,12 @@
+class Player: 
+    """Creates the player of the card game
+    
+    Arrtibutes: 
+        name(str):
+        order(int):
+    """
+    def __init__(self, name, order): 
+        self.name = name
+        self.order = order
+        
+        
