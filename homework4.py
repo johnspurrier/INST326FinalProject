24d3e7fdb15e45e8715a5960331f445a23fcad05
Chloe Root <croot1@umd.edu>
2021-11-26 13:41:05 -0500

    yes

--- homework4.py
+++ homework4.py
@@ -6,19 +6,33 @@ class Book:
     """
     Attributes: 
         callnum(str): a string containing the book’s Library of Congress call number
-        title(): a string containing the title of the book
-        author(): a string containing the name of the book’s author(s); if the author is
+        title(str): a string containing the title of the book
+        author(str): a string containing the name of the book’s author(s); if the author is
         unknown, this should be an empty string
     """
     def __init__(self, callnum, title, author): 
         self.callnum = callnum
         self.title = title
         self.author = author 
-        book1 = [callnum, title, author]
-        book2 = [callnum, title, author]
     
     def __repr__(self): 
       print(f"Book{self.callnum!r}, {self.title!r}, {self.author!r}")
+    
+    def __lt__(self, other):
+        if self < other:
+            return True
+        
+    def __lte__(self, other): 
+        if self <= other:
+            return True
+        
+    def __gt__(self, other): 
+        if self > other:
+            return True
+    
+    def __gte__(self, other): 
+        if self >= other: 
+            return True
     
 def read_books(file): # use regular expressions
     with open(file, 'r', encoding='utf-8') as f: 
