8d090773b1cb46f5de5d73eb532cfa9e72ab3805
Chloe Root <croot1@umd.edu>
2021-12-01 09:25:28 -0500

    yes

--- homework4.py
+++ homework4.py
@@ -11,9 +11,22 @@ class Book:
         is unknown, this should be an empty string
     """
     def __init__(self, callnum, title, author): 
-        self.callnum = callnum
-        self.title = title
-        self.author = author 
+        expr = r"[A-Z]{1,3}\d{1,4}(\.?)(\d{1,4})?(\s?\.?\w?\d+)\s\d{1,4}"
+    # First section: ([A-Z]{1,3})
+    # Second section: (\d{1,4}(\.?)(\d{1,4})?)
+    # Third section: (\s?\.?\w?\d+)\s
+    # Fourth section: \d{1,4}
+        match = re.search(expr,)
+        if match == None:
+            raise ValueError("Address cannot be parsed")
+        else:
+            self.callnum = callnum
+            self.no1 = match.group(1)
+            self.no2 = match.group(2)
+            self.no3 = match.group(3)
+            self.no4 = match.group(4)
+            self.title = title
+            self.author = author 
     
     def __repr__(self): 
       print(f"Book{self.callnum!r}, {self.title!r}, {self.author!r}")
@@ -30,12 +43,14 @@ class Book:
         else: 
             return False
     
-def read_books(file): # use regular expressions
+def read_books(file):
     with open(file, 'r', encoding='utf-8') as f: 
-        for line in f: 
-            line.strip()
+        numlist = [Book(callnum)for callnum in f]
+        return numlist.strip()
+            
             
 def main(file): 
+    file.sort()
     return 
     
 def parse_args(list1):
@@ -43,6 +58,7 @@ def parse_args(list1):
 
 if __name__ == "__main__":
     main()
+    
     
     
             
